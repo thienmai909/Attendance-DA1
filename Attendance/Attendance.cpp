7096e249f7b72ab48191bedd20072b55ab2bc2a1
Thien Mai <thienmai0637@gmail.com>
2026-03-27 22:29:48 +0700

    Loại thư mục data

--- Attendance/Attendance.cpp
+++ Attendance/Attendance.cpp
@@ -9,8 +9,20 @@ int main() {
     AppManager attendance;
     attendance.khoiDong();
 
-    auto dsSV = attendance.getSVManager();
-    dsSV.them(SinhVien("0023412316", "Mai Hữu Thiện"));
-    if (dsSV.timTheoMa("023412316").has_value())
-        std::cout << std::format("tìm thấy");
+    auto& dsSV = attendance.getSVManager();
+    auto& dsGV = attendance.getGVManager();
+
+    try {
+        GiangVien gv1 = GiangVien("0023412316", "Nguyễn Ngọc Chi");
+        gv1.setTaiKhoan("ngocchi", "ngocchi123");
+        gv1.setHocVi(Degree::THACSI);
+        dsGV.capNhat(gv1);
+
+    } catch (const std::invalid_argument& e) {
+        std::cout << e.what();
+    }
+    // if (dsSV.timTheoMa("0023412244").has_value())
+    //     std::cout << std::format("tìm thấy");
+    // dsSV.saveIfDirty();
+    attendance.dongLai();
 }
